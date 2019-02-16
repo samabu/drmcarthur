3d0b44e6654270ece016de12807db588aa36710b
samabu <samuelalbertjohnston@gmail.com>
2019-02-16 12:16:18 -0700

    home page start

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <nav>
+          <ul className="navUL">
+            <li className="navLI"><a className="navLinks" href="#">BOOK AN APPOINTMENT</a></li>
+            <li className="navLI"><a className="navLinks" href="#">MAKE A PAYMENT</a></li>
+            <li className="navLI"><a className="navLinks" href="#">ABOUT US</a></li>
+            <li className="navLI"><a className="navLinks" href="#">Contact</a></li>
+          </ul>
+        </nav>
+        <div className="Welcome">
+          <div className="welcome_banner">
+            <div className="welcome_logo"></div>
+          </div>
+        </div>
+        <div className="Wood">
+
+        </div>
       </div>
     );
   }
